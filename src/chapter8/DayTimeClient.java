d16fb61897b8185e118825d20554c9f67430d1df
eliotjang <eliotjang2@gmail.com>
2019-10-15 14:10:43 +0900

    add DayTimeClient

---
+++ src/chapter8/DayTimeClient.java
@@ -0,0 +1,31 @@
+package chapter8;
+
+import java.io.*;
+import java.net.*;
+
+public class DayTimeClient {
+	
+	public static void main(String[] args) {
+		Socket theSocket;
+		String host;
+		InputStream is;
+		BufferedReader reader;
+		if(args.length > 0) {
+			host = args[0];
+		}else {
+			host = "localhost";
+		}
+		try {
+			theSocket = new Socket(host, 50000);
+			is = theSocket.getInputStream();
+			reader = new BufferedReader(new InputStreamReader(is));
+			String theTime = reader.readLine();
+			System.out.println("호스트의 시간은 " + theTime + "이다");
+		}catch(UnknownHostException e) {
+			System.err.println(args[0] + " 호스트를 찾을 수 없습니다.");
+		}catch(IOException e) {
+			System.err.println(e);
+		}
+	}
+	
+}
