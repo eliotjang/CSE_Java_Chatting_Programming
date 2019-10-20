3b55ddcd166ed6bec039adf210ab6b91ed96b954
eliotjang <eliotjang2@gmail.com>
2019-10-20 12:26:40 +0900

    resloved enclosing error in MyEchoServer

--- src/chapter7/MyEchoServer.java
+++ src/chapter7/MyEchoServer.java
@@ -4,41 +4,6 @@ import java.io.*;
 import java.net.*;
 
 public class MyEchoServer {
-	
-	class Echo extends Thread{
-		
-		public Echo(Socket connection) {
-			InputStream is;
-			BufferedReader reader;
-			OutputStream os;
-			BufferedWriter writer;
-			String theLine;
-			
-			try {
-				is = connection.getInputStream();
-				reader = new BufferedReader(new InputStreamReader(is));
-				os = connection.getOutputStream();
-				writer = new BufferedWriter(new OutputStreamWriter(os));
-				
-				while((theLine = reader.readLine()) != null) {
-					System.out.println(theLine);
-					writer.write(theLine + '\r' + '\n');
-					writer.flush();
-				} 
-			} catch(IOException e) {
-				System.err.println(e.toString());
-			} finally {
-				if(connection != null) {
-					try {
-						connection.close();
-					} catch (IOException e) {
-						System.err.println(e.toString());
-					}
-				}
-			}
-		
-		}
-	}
 
 	public static void main(String[] args) {
 		ServerSocket theServer;
@@ -53,15 +18,10 @@ public class MyEchoServer {
 			
 			while(true) {
 				Socket connection = null;
-				Echo client = null;
-				try {
-					connection = theServer.accept();
-					client = new Echo(connection);
-					client.start();
-				} catch (Exception e) {
-					System.out.println(e.toString());
-				}
-				
+				Echo client = null;		
+				connection = theServer.accept();
+				client = new Echo(connection);
+				client.run();							
 			}
 		} catch (UnknownHostException e) {
 			System.err.println(e.toString());
@@ -70,4 +30,39 @@ public class MyEchoServer {
 		} 
 	}
 
+}
+
+class Echo extends Thread{
+	
+	public Echo(Socket connection) {
+		InputStream is;
+		BufferedReader reader;
+		OutputStream os;
+		BufferedWriter writer;
+		String theLine;
+		
+		try {
+			is = connection.getInputStream();
+			reader = new BufferedReader(new InputStreamReader(is));
+			os = connection.getOutputStream();
+			writer = new BufferedWriter(new OutputStreamWriter(os));
+			
+			while((theLine = reader.readLine()) != null) {
+				System.out.println(theLine);
+				writer.write(theLine + '\r' + '\n');
+				writer.flush();
+			} 
+		} catch(IOException e) {
+			System.err.println(e.toString());
+		} finally {
+			if(connection != null) {
+				try {
+					connection.close();
+				} catch (IOException e) {
+					System.err.println(e.toString());
+				}
+			}
+		}
+	
+	}
 }
