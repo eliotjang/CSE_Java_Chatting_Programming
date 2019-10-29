04b72485dec4e3d3c5a09aef492af3a29cf73289
eliotjang <eliotjang2@gmail.com>
2019-10-29 20:02:23 +0900

    add FileCopy java

--- src/chapter4/FileCopy.java
+++ src/chapter4/FileCopy.java
@@ -12,12 +12,20 @@ public class FileCopy {
 		File file = new File("Exam_02.txt");
 		FileReader fin = new FileReader(file);
 		FileWriter fw = new FileWriter("Exam_02_1.txt");
-		BufferedReader br = new BufferedReader(fin);
 		
-		String line = Integer.toString(data);
+		BufferedReader br = new BufferedReader(fin);
+		//String line = Integer.toString(data);
+		String line = String.valueOf(data);
 		while((line=br.readLine()) != null) {
 			System.out.println(line);
 		}
+		
+		//data가 스트링을 바뀌었을 경우
+		
+		/*BufferedReader br = new BufferedReader(fin);
+		while((data=br.readLine(buffer)) > -1) {
+			System.out.println(data);
+		}*/
 		
 		/*while((data = fin.read(buffer)) > -1) {
 			fw.write(buffer,0,data);
