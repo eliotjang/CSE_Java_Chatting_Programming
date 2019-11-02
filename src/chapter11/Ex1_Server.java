590805ae017ba52bf98953ff3759d8263e3cae37
eliotjang <eliotjang2@gmail.com>
2019-11-02 13:36:57 +0900

    update chapter11

---
+++ src/chapter11/Ex1_Server.java
@@ -0,0 +1,45 @@
+package chapter11;
+
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+
+public class Ex1_Server {
+   public final static int daytimeport=5099;
+   public final static int BUFFER_SIZE=9000;
+   public static void main(String args[]) {
+      DatagramSocket theSocket;
+      String message;
+      byte[] buffer = new byte[BUFFER_SIZE];
+      try {
+         theSocket = new DatagramSocket(daytimeport);
+         
+            try {
+               while(true) {
+               DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+               theSocket.receive(incoming);
+               
+               Date now = new Date();
+               message = now.toString()+"\r\n";
+               System.out.println(incoming.getAddress()+ "와 연결되었습니다.");
+               System.out.println(message);
+               buffer = message.getBytes();
+               DatagramPacket outgoing = new DatagramPacket(buffer, buffer.length, incoming.getAddress(), incoming.getPort());
+               theSocket.send(outgoing);
+               }
+            }catch(IOException e) {
+               System.out.println(e);
+            }finally {
+               if(theSocket != null)theSocket.close();
+            }
+               
+      }catch(IOException e) {
+         System.out.println(e);
+         
+      }
+   }
+}      
+         
+         
+         
+      
---
+++ src/chapter11/One2OneClient.java
@@ -0,0 +1,20 @@
+package chapter11;
+
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class One2OneClient extends Frame implements ActionListener {
+	public One2OneClient() {
+		
+	}
+	
+	public void watForPackets() {
+		while(true) {
+			
+		}
+	}
+	
+	public void actionPerformed( )
+}
